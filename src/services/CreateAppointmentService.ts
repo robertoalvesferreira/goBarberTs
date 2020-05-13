cb5716de03af03fe2f55a223a8779930bd61188e
robertoalvesferreira <robertoaff06@gmail.com>
2020-05-13 20:55:09 -0300

    Projeto iniciado, já com duas tabelas 'appointments' e 'user' e com suas respectivas rotas e models pré configurados

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,43 @@
+import { startOfHour } from 'date-fns';
+
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * [X]Recebimento das informacoes
+ * [X]Tratativa de error e erros/execoes
+ * [X]Acesso ao repositorio de appointments
+ */
+interface Request {
+  provider: string;
+  date: Date;
+}
+/**
+ * Dependency Inversion (SOLID)
+ */
+class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+export default CreateAppointmentService;
