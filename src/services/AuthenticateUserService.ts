cfb7ccf75bf72d3d3900416e324d605031cf3beb
robertoalvesferreira <robertoaff06@gmail.com>
2020-05-13 22:36:13 -0300

    Configurando JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -26,10 +28,15 @@ class AuthenticateUserService {
     if (!passswordMatched) {
       throw new Error('Incorrect email/password combination.');
     }
-    // usuario autenticado
+
+    const token = sign({}, 'aa12kaspojym53155fgsfga', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
+      token,
     };
   }
 }
