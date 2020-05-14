5c031bae8b8c4939ff2791866c00bf94f454540c
robertoalvesferreira <robertoaff06@gmail.com>
2020-05-13 22:07:38 -0300

    Criando rotas e services de sesssão

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,36 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passswordMatched = await compare(password, user.password);
+
+    if (!passswordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+    // usuario autenticado
+
+    return {
+      user,
+    };
+  }
+}
+export default AuthenticateUserService;
